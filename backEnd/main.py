1ca4ceca9af52cb891a1f3d77d8ebab6dbe73179
zirigui <guilherme15924@gmail.com>
2025-04-28 20:09:10 -0300

    Adicionando CORS

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.routers import users, reservas, teste
 from app.database import get_db_connection
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(users.router, prefix="/auth")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://200.132.38.218:3047"],  # Permite apenas esse IP
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
+
+app.include_router(users.router)
 app.include_router(reservas.router)
 
 @app.get("/")
