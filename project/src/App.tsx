94f7711383c4960e5f96a53ec01e9d72423c5895
zirigui <guilherme15924@gmail.com>
2025-05-15 17:52:13 -0300

    Correção de erro de notices

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -44,7 +44,7 @@ function App() {
     if (screen === 'myBookings') window.location.href = '/my-bookings';
     if (screen === 'replays') window.location.href = '/replays';
     if (screen === 'admin') window.location.href = '/admin'
-    if (screen === 'avisos') window.location.href = '/avisos'
+    if (screen === 'notices') window.location.href = '/avisos'
   };
 
   return (
