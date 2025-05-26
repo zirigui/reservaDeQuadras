153017860d11d2be635550f69e10e042f9973af1
zirigui <guilherme15924@gmail.com>
2025-05-26 19:01:03 -0300

    Ajuste na estilização

--- project/src/components/sidebar/Sidebar.tsx
+++ project/src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {
     SidebarContainer,
-    MenuItem
+    MenuItem,
+    MenuGroup,
+    LogoutButton
   } from './SidebarStyles';
 
 
@@ -12,13 +14,17 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ onNavigate }) => {
   return (
     <SidebarContainer>
-      <MenuItem onClick={() => onNavigate('admin')}>Admin</MenuItem>
-      <MenuItem onClick={() => onNavigate('notices')}>Avisos</MenuItem>
-      <MenuItem onClick={() => onNavigate('booking')}>Reservar quadra</MenuItem>
-      <MenuItem onClick={() => onNavigate('myBookings')}>Meus agendamentos</MenuItem>
-      <MenuItem onClick={() => onNavigate('notifications')}>Notificações</MenuItem>
+      
+      <MenuGroup>
+        <MenuItem onClick={() => onNavigate('admin')}>Admin</MenuItem>
+        <MenuItem onClick={() => onNavigate('notices')}>Avisos</MenuItem>
+        <MenuItem onClick={() => onNavigate('booking')}>Reservar quadra</MenuItem>
+        <MenuItem onClick={() => onNavigate('myBookings')}>Meus agendamentos</MenuItem>
+        <MenuItem onClick={() => onNavigate('notifications')}>Notificações</MenuItem>
+      </MenuGroup>
+      
       <MenuItem onClick={() => onNavigate('settings')}>Configurações</MenuItem>
-      <MenuItem onClick={() => onNavigate('logout')}>Sair</MenuItem>
+      <LogoutButton onClick={() => onNavigate('logout')}>Sair</LogoutButton>
     </SidebarContainer>
   );
 };
